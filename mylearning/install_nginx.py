48fb8f4165ff0d06bc0a1ee1cb47260d1a60a94f
search4sai <saiprakash.sep09@gmail.com>
2025-02-20 09:41:51 +0530

    update nginx code

--- mylearning/install_nginx.py
+++ mylearning/install_nginx.py
@@ -1,21 +1,24 @@
 import os
 import subprocess
-from install_apache import IndexUpdater
-from install_apache import install_package
+from install_apache import IndexUpdater, ApacheOperations
 
 def install_nginx():
     try:
         # Update the package list
         subprocess.run(['sudo', 'apt-get', 'update'], check=True)
         
-        # Install nginx using install_package function from install_apache
-        install_package('nginx')
+        # Create an instance of ApacheOperations to use the install_package method
+        apache_ops = ApacheOperations()
+        
+        # Install nginx using install_package method from ApacheOperations
+        apache_ops.install_package('nginx')
         
         print("Nginx installed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
         # Update the index.html file
         index_updater = IndexUpdater()
-        index_updater.update_index()
+        index_updater.update_index_html()
+
 if __name__ == "__main__":
     install_nginx()
